aca2a07374f619bfe9d90cbfa5a80d86ab67422a
melodyfff <307208327@qq.com>
2021-01-05 16:23:07 +0800

    :smile: update

--- src/main/java/com/xinchen/tool/Main.java
+++ src/main/java/com/xinchen/tool/Main.java
@@ -8,36 +8,50 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
+
 /**
  * Hello world!
  *
  */
 public class Main
 {
-    static final int LOCAL_PORT = Integer.parseInt(System.getProperty("localPort", "80"));
+    static final int LOCAL_PORT = Integer.parseInt(System.getProperty("localPort", "8080"));
     static final String REMOTE_HOST = System.getProperty("remoteHost", "localhost");
-    static final int REMOTE_PORT = Integer.parseInt(System.getProperty("remotePort", "80"));
+    static final int REMOTE_PORT = Integer.parseInt(System.getProperty("remotePort", "1323"));
 
 
-    public static void main(String[] args) throws InterruptedException {
-        System.err.println("Proxying *:" + LOCAL_PORT + " to " + REMOTE_HOST + ":" + REMOTE_PORT + " ...");
-
+    public static void main(String[] args){
         //Configure the bootstrap
-
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workGroup = new NioEventLoopGroup();
 
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new ProxyInitializer(REMOTE_HOST, REMOTE_PORT))
-                    .childOption(ChannelOption.AUTO_READ, false)
-                    .bind(LOCAL_PORT).sync().channel().closeFuture().sync();
-        } finally {
-            bossGroup.shutdownGracefully();
-            workGroup.shutdownGracefully();
-        }
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup2 = new NioEventLoopGroup();
+
+        proxy(bossGroup,workGroup,8081,REMOTE_HOST,REMOTE_PORT);
+        proxy(bossGroup,workGroup2,8082,REMOTE_HOST,REMOTE_PORT);
+    }
+
+    static void proxy(EventLoopGroup bossGroup,
+                      EventLoopGroup workGroup,
+                      int bindPort,
+                      String remoteHost,
+                      int remotePort) {
+        new Thread(()->{
+            System.err.println("Proxying *:" + bindPort + " to " + remoteHost + ":" + remotePort + " ...");
+            try {
+                ServerBootstrap b = new ServerBootstrap();
+                b.group(bossGroup, workGroup)
+                        .channel(NioServerSocketChannel.class)
+                        .handler(new LoggingHandler(LogLevel.INFO))
+                        .childHandler(new ProxyInitializer(remoteHost, remotePort))
+                        .childOption(ChannelOption.AUTO_READ, false)
+                        .bind(bindPort).sync().channel().closeFuture().sync();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                bossGroup.shutdownGracefully();
+                workGroup.shutdownGracefully();
+            }
+        }).start();
     }
 }
